96004f2cda805706e895fa246456d6daf0437efd
rumeysakdogan <dgn.rumeysaa@gmail.com>
2020-11-09 15:15:32 -0700

    added remote-chrome for Selenium Grid

--- src/test/java/com/zerobank/utilities/Driver.java
+++ src/test/java/com/zerobank/utilities/Driver.java
@@ -4,7 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -26,6 +29,17 @@ public class Driver {
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
+                    break;
+                case "remote-chrome":
+                    try{
+                        //ChromeOptions chromeOptions = new ChromeOptions();
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        URL gridUrl = new URL("http://3.82.5.142:4444/wd/hub");
+                        driver = new RemoteWebDriver(gridUrl,desiredCapabilities);
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     throw new RuntimeException("No such a browser yet.");
